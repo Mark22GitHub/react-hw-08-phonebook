039dbc46d51a87e0c7cfbafb9cce0d410ab1b281
Mark22GitHub <atelezhenko93@gmail.com>
2021-03-24 00:47:11 +0200

    day 1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import AppBar from "./Components/AppBar/AppBar";
 import HomePage from "./pages/homePage/HomePage";
+import ContactsPage from "./pages/contactsPage/ContactsPage";
 import RegisterPage from "./pages/registerPage/RegisterPage";
 import LoginPage from "./pages/loginPage/LoginPage";
-import ContactsPage from "./pages/contactsPage/ContactsPage";
 
 const App = () => (
   <>
@@ -12,9 +12,9 @@ const App = () => (
 
     <Switch>
       <Route exact path="/" component={HomePage} />
+      <Route path="/contacts" component={ContactsPage} />
       <Route path="/register" component={RegisterPage} />
       <Route path="/login" component={LoginPage} />
-      <Route path="/contacts" component={ContactsPage} />
     </Switch>
   </>
 );
--- src/Components/AppBar/AppBar.js
+++ src/Components/AppBar/AppBar.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "../AppBar/AppBar.module.css";
 import Navigation from "../Navigation/Navigation";
-import UserMenu from "../UserMenu/UserMenu";
 import AuthNav from "../AuthNav/AuthNav";
+import UserMenu from "../UserMenu/UserMenu";
 
 const AppBar = ({ isAuthenticated }) => (
   <header className={styles.header}>
